cfc627a458f36013773e8a729b314725bd8d5869
Onkar Kumar <63390852+onkar4580@users.noreply.github.com>
2022-09-02 14:19:52 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,37 @@
+let c1=0;
+let c2=0;
+
+
+const score1 = document.getElementById('h1');
+const score2 = document.getElementById('h2');
+
+
+function inc1() {
+    c1+=1;
+    score1.textContent = c1;
+}
+
+function inc2() {
+    c1+=2;
+    score1.textContent = c1;
+}
+
+function inc3() {
+    c1+=3;
+    score1.textContent = c1;
+}
+
+function inc_1() {
+    c2+=1;
+    score2.textContent = c2;
+}
+
+function inc_2() {
+    c2+=2;
+    score2.textContent = c2;
+}
+
+function inc_3() {
+    c2+=3;
+    score2.textContent = c2;
+}
